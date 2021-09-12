1d22685a7b68a93d10934326c4836b171df93d1b
JaeHeee <kjha2142@gmail.com>
2021-09-12 15:30:00 +0900

    feat: Add firebase Crashlytics example

--- lib/firebase/firebase_crashlytics.dart
+++ lib/firebase/firebase_crashlytics.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
 class FirebaseCrashlyticsScreen extends StatelessWidget {
@@ -11,7 +15,78 @@ class FirebaseCrashlyticsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Firebase Crashlytics'),
       ),
-      body: Container(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                FirebaseCrashlytics.instance
+                    .setCustomKey('setCustomKeyTest', 'customValue');
+              },
+              child: const Text('Key'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                FirebaseCrashlytics.instance.log('log Test');
+              },
+              child: const Text('Log'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                FirebaseCrashlytics.instance.crash();
+              },
+              child: const Text('Crash'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                throw StateError('Uncaught error thrown by app');
+              },
+              child: const Text('Throw Error'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                runZonedGuarded(() {
+                  Future<void>.delayed(const Duration(seconds: 2), () {
+                    final list = <int>[];
+                    print(list[0]);
+                  });
+                }, FirebaseCrashlytics.instance.recordError);
+              },
+              child: const Text('runZonedGuarded'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                try {
+                  throw Error();
+                } catch (exception, stack) {
+                  await FirebaseCrashlytics.instance.recordError(
+                    exception,
+                    stack,
+                    reason: 'Test fatal error',
+                    fatal: true,
+                  );
+                }
+              },
+              child: const Text('Record Fatal Error'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                try {
+                  throw Error();
+                } catch (exception, stack) {
+                  await FirebaseCrashlytics.instance.recordError(
+                    exception,
+                    stack,
+                    reason: 'as an example of non-fatal error',
+                  );
+                }
+              },
+              child: const Text('Record Non-Fatal Error'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
