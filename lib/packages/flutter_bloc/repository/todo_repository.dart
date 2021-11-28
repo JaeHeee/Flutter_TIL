ab38c6f3356a06ca8d2101e648c1d86f6c3ec3d8
JaeHeee <kjha2142@gmail.com>
2021-11-28 17:30:05 +0900

    feat: Add todo repository

---
+++ lib/packages/flutter_bloc/repository/todo_repository.dart
@@ -0,0 +1,32 @@
+import '../model/todo.dart';
+
+class TodoRepository {
+  Future<List<Map<String, dynamic>>> listTodo() async {
+    await Future.delayed(const Duration(seconds: 1));
+
+    return [
+      {
+        'id': 1,
+        'title': 'Flutter',
+        'createdAt': DateTime.now().toString(),
+      },
+      {
+        'id': 2,
+        'title': 'Dart',
+        'createdAt': DateTime.now().toString(),
+      },
+    ];
+  }
+
+  Future<Map<String, dynamic>> createTodo(Todo todo) async {
+    await Future.delayed(const Duration(seconds: 1));
+
+    return todo.toJson();
+  }
+
+  Future<Map<String, dynamic>> deleteTodo(Todo todo) async {
+    await Future.delayed(const Duration(seconds: 1));
+
+    return todo.toJson();
+  }
+}
